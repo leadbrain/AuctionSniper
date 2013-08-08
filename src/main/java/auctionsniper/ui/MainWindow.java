2f939abd6c9d304f377f370cbda1844e5ab3c6a7
leadbrain <leadbrain@gmail.com>
2013-08-09 01:49:01 +0900

    11.2.3까지 완료

---
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -0,0 +1,30 @@
+package auctionsniper.ui;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
+
+import com.leadbrain.ex.ApplicationRunner;
+import com.leadbrain.ex.Main;
+
+public class MainWindow extends JFrame {
+    private final JLabel sniperStatus = createLabel(ApplicationRunner.STATUS_JOINING);
+
+    public MainWindow() {
+        super("Auction Sniper");
+        setName(Main.MAIN_WINDOW_NAME);
+        add(sniperStatus);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    private static JLabel createLabel(String initialText) {
+        JLabel result = new JLabel(initialText);
+        result.setName(Main.SNIPER_STATUS_NAME);
+        result.setBorder(new LineBorder(Color.BLACK));
+        return result;
+    }
+
+}
